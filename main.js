46b0601c80246100abda87ae6f26e64664b04ee1
Siddhant Singh <siddhant06137@gmail.com>
2023-04-15 23:25:16 +0530

    added gitignore and fixed issue for getting data from excel file

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 let publicationsPath = "./data/publications.xlsx";
+async function readFile() {
+    try {
+        // Load the XLSX file using fetch
+        const response = await fetch(publicationsPath);
+        const data = await response.arrayBuffer();
 
-(async () => {
-    let workbook = XLSX.read(
-        await (await fetch(publicationsPath)).arrayBuffer()
-    );
-    console.log(workbook);
-})();
+        // Parse the XLSX file
+        const workbook = XLSX.read(data, { type: "arraybuffer" });
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+        // return jsonData;
+        // console.log(jsonData[1][0]);
+
+        // Display the data in a table
+        const dataTable = document.getElementById("dataTable");
+        dataTable.innerHTML = "";
+        jsonData.forEach((row) => {
+            const tr = document.createElement("tr");
+            row.forEach((cell) => {
+                const td = document.createElement("td");
+                td.textContent = cell;
+                tr.appendChild(td);
+            });
+            dataTable.appendChild(tr);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+readFile();
