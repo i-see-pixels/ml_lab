974412e4265437b112d565cf19463005ec84db6a
Siddhant Singh <siddhant06137@gmail.com>
2023-05-29 01:43:46 +0530

    changed about page

--- main.js
+++ main.js
@@ -66,7 +66,11 @@ function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-const phrases = ["Machine Learning", "Computer Vision"];
+const phrases = [
+    "Machine Learning",
+    "Computer Vision",
+    "Artificial Intelligence",
+];
 const el = document.getElementById("typewriter");
 
 let sleepTime = 100;
